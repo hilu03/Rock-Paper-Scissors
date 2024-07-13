d54b3baef722125ab260ec24c47709a3dc0d0595
Luong Tran <tranthihienluong2003@gmail.com>
2024-07-13 11:16:50 +0700

    Create playRound function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let humanScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice() {
   let randomNumber = Math.random();
   let result;
@@ -10,15 +14,61 @@ function getComputerChoice() {
   else {
     result = 'scissors';
   }
-  console.log(randomNumber);
-  console.log(result);
+  console.log('Computer choice: ' + result);
   return result;
 }
 
 function getHumanChoice() {
   let result = prompt("Input your choice (rock, paper, scissors): ");
-  console.log(result);
+  console.log('Your choice: ' + result);
   return result.toLowerCase();
 }
 
-getHumanChoice();
+function playRound(humanChoice, computerChoice) {
+  let result;
+  if (humanChoice == 'rock') {
+    if (computerChoice == 'scissors') {
+      result = 'You win! Rock beats Scissors.';
+      humanScore += 1;
+    }
+    else if (computerChoice == 'paper') {
+      result = 'You lose! Paper beats Rock.';
+      computerScore += 1;
+    }
+    else {
+      result = 'Tie!';
+    }
+  }
+  else if (humanChoice == 'paper') {
+    if (computerChoice == 'scissors') {
+      result = 'You lose! Scissors beats Paper.';
+      computerScore += 1;
+    }
+    else if (computerChoice == 'rock') {
+      result = 'You win! Paper beats Rock.';
+      humanScore += 1;
+    }
+    else {
+      result = 'Tie!';
+    }
+  }
+  else if (humanChoice == 'scissors') {
+    if (computerChoice == 'rock') {
+      result = 'You lose! Rock beats Scissors.';
+      computerScore += 1;
+    }
+    else if (computerChoice == 'paper') {
+      result = 'You win! Scissors beats Paper.';
+      humanScore += 1;
+    }
+    else {
+      result = 'Tie!';
+    }
+  }
+  console.log(result);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
